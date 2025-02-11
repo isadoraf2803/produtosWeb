138dd54be53c5ab27cbd059cc034d786ed13aa1a
gilvan-Borges <gilvan2022borges@gmail.com>
2025-02-11 12:49:54 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { CadastroProdutosComponent } from './components/pages/cadastro-produtos/cadastro-produtos.component';
+import { EdicaoProdutosComponent } from './components/edicao-produtos/edicao-produtos.component';
+import { ConsultarProdutosComponent } from './components/consultar-produtos/consultar-produtos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'pages/cadastro-produtos',
+        component: CadastroProdutosComponent
+        
+    },
+    {
+        path: 'pages/consulta-produtos',
+        component: ConsultarProdutosComponent
+    },
+    {
+        path: 'pages/edicao-produtos',
+        component: EdicaoProdutosComponent
+    }
+];
